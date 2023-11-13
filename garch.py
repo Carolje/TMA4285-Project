ce4c3583cc57028f529fbe4084a0877152042d9d
carolje <caro-line_99@hotmail.com>
2023-11-13 15:44:24 +0100

    garch pred

--- garch.py
+++ garch.py
@@ -4,6 +4,7 @@ from scipy.optimize import minimize
 from numpy.linalg import norm
 import dataTransformations as dF
 import arch
+import numpy.random as nprand
 
 #Get data and difference data
 df=pd.read_csv("Data1997-2022.csv")
@@ -30,10 +31,12 @@ def sigma_t_l(r_prev,sigma_prev,params,p,q,m,covs):
     r_prev: the returns from the current and previous timesteps
     sigma_prevs: the sigma estimates for previous time steps
     params: estimated paramameters for alphas, betas and gammas
-    p: 
-    q:
+    p: The number of return regressors
+    q: The number of sigma regressors
     m: the number of covariates
     covs: matrix of covariates for the current timestep
+
+    returns: The new calculated sigma value. 
     """
     
     r_prev = np.append(r_prev,1)
@@ -44,13 +47,30 @@ def sigma_t_l(r_prev,sigma_prev,params,p,q,m,covs):
         a+=params[i]*r_prev[-i]**2
     for i in range(q):
         b+=params[p+i]*sigma_prev[-i]**2
-    for i in range(m):
-        g+=params[p+q+i]*covs[i]
-    return np.sqrt(a+b+g)
+    # for i in range(m):
+    #     g+=params[p+q+i]*covs[i]
+    g=params[p+q+i:]@covs
+    sum=a+b+g
+    return np.sqrt(sum)
 
 
 def logLikelihood(P,*args):
-    """ Function that calculates and 
+    """ Function that calculates the log likelihood.
+
+    Parameters:
+    P: A (p+q+m)x1) vector with parameter estimates for alphas, betas and gammas. The parameters inn
+        these vector gets estimated by MLE.
+    *args:
+    r: A (px1) vector of previous return values.
+    sigma: A (q+1) vector with the sigma estimates for previous time steps
+    covs: A (1xm) matrix of covariates for the current timestep
+    t: The current timestep
+    n_a: The number of alphas
+    n_b: The number of betas
+
+    return: the calculated log-likelihood
+    
+
     """
     r,sigma,covs,t,n_a,n_b=args
     r_new = [r[i] for i in range(len(r)-1,-1,-1)]
@@ -65,7 +85,6 @@ def logLikelihood(P,*args):
     l=-np.log(L)
     return l
 
-#SUSAN ZONE
 def score_1(P,*args):
     """
     Computes the score vector of the log-likelihood of a GARCH(q,p)-model with respect to the vector
@@ -89,8 +108,31 @@ def score_1(P,*args):
     return score
 
 def con_pos(x):
+    "Constraint for the minimizer specifying positive parameters. "
     return np.abs(x)
+
 def garch_fit(alphas_init,betas_init,tol,r,covs,maxiter,p,q,m,sigma_init,gammas_init,N):
+    """ Function for fitting a GARCHX(p,q) model and estimating parameters.
+
+    Paramaters:
+    alphas_init: A ((p+1)x1) vector with initial guesses for the alphas
+    betas_init: A (qx1) vector with initial guesses for the betas
+    tol: Tolerance for allowed error, int.
+    r: A (Nx1) vector with values for the returns
+    covs: A (Nx3) vector with the data for the covariates
+    maxiter: The maximum number of allowed iterations, int.
+    p: The number of return regressors
+    q: The number of sigma regressors
+    m: the number of covariates
+    sigma_init: Initial value for sigma, int
+    gammas_init: A (3x1) vector with initial values for the gammas
+    N: Total number of returns, int
+
+    Fits a GARCH model with m covariates and estimates parameters for alphas, betas and gammas by using MLE.
+    The function terminates when the change in variables are less than the toleranse.
+
+    Returns: The estimated parameters, number of iterations, the returns used and the estimated sigmas.
+    """
     not_tol=True
     i=0
     sigma_prevs=np.array([sigma_init])
@@ -102,9 +144,7 @@ def garch_fit(alphas_init,betas_init,tol,r,covs,maxiter,p,q,m,sigma_init,gammas_
     cons={"type":"eq","fun":con_pos}
     while not_tol and i<maxiter:
         c=covs[i,:]
-        #print("c",covs[i,:])
         sigma=sigma_t_l(r_prevs,sigma_prevs,params_old,p,q,m,c)
-        #print(sigma)
         #minimize log likelihood and get parameter estimates
         if((len(params_old)-4)>(len(r_prevs)+len(sigma_prevs))):
             m=len(params_old)-3-(len(r_prevs)+len(sigma_prevs))
@@ -138,10 +178,10 @@ def garch_fit(alphas_init,betas_init,tol,r,covs,maxiter,p,q,m,sigma_init,gammas_
         i+=1
         r_prevs=np.append(r_prevs,r[i])
         sigma_prevs=np.append(sigma_prevs,sigma)
-    #If yes break loop, if not continue to iterate and smaller than maxiter
     return params_old,i,sigma_prevs,r_prevs
 
-
+p=2
+q=2
 params,i,sigma_prevs,r_prevs=garch_fit(alphas_init=np.array([0.005,0.005,0.005]),betas_init=np.array([0.005,0.005]),tol=1e-7,r=r,covs=covs,maxiter=100,p=2,q=2,m=3,sigma_init=0.05,gammas_init=np.array([0.01,0.01,0.01]),N=len(r))
 print("params=",params)
 print("i",i)
@@ -152,3 +192,18 @@ print("sigma_prevs",sigma_prevs)
 # results=model.fit()
 # print(results)
 
+def predict_garch(params, r_prev, sig_prev, covs_prev, M, npred, p, q):
+    r_pred=np.copy(r_prev)
+    print(r_pred)
+    for i in range(npred):
+        yeet_r = np.flip(r_pred)
+        yeet_sig = np.flip(sig_prev)
+        sigma=sigma_t_l(r_prevs,sigma_prevs,params,p,q,3,covs[-1,:])
+        mu=0
+        preds = nprand.normal(mu,sigma,M)
+        mp=np.mean(preds)
+        r_pred = np.append(r_pred,mp)
+    return r_pred
+
+preds=predict_garch(params, r_prevs, sigma_prevs, covs, 100, 5, p, q)
+print(preds)
